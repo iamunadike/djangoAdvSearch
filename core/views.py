8733712a9fa3195e2ecf05f9b9fb7f8b015d784c
smeez <gatez@yahoo.com>
2020-06-17 11:36:23 +0100

    view action for form rendering

--- core/views.py
+++ core/views.py
@@ -8,14 +8,18 @@ from django.db.models import Q
 @require_http_methods(['GET'])
 def SimpleForm(request):
     qs = Journal.objects.all()
-    categories = Catgory.objects.all()
-    title_query = request.GET.get('title')
+    categories = Category.objects.all()
+    if not 'reviewed' in request.GET:
+        qs = qs.filter(reviewed=False)
+    else:
+        factor = request.GET.get("reviewed") == "on"
+        qs = qs.filter(reviewed=factor)
     queries_ = dict(six.iteritems((request.GET)))
-    queries = ((k,v) for k,v in dict.items(queries_) if len(v) != 0 and v != None)
+    queries = ((k,v) for k,v in dict.items(queries_) if len(v) != 0 and v != None and k is not "reviewed")
+    
     while 1:
         try:
             x = queries.__next__()
-            print("this is x => ", x[0], x[1])
             if x[0].__contains__("title") and not  x[0].__contains__("author"):
                 qs = qs.filter(title__icontains=x[1])
             if x[0].__contains__("title") and x[0].__contains__("author"):
@@ -23,13 +27,12 @@ def SimpleForm(request):
             if x[0].__contains__('date'):
                 y = queries.__next__()
                 qs = qs.filter(Q(published__gte=x[1]) & Q(published__lt=y[1]))
-            if x[0].__contains__('view'):
+            if x[0].startswith('view'):
                 z = queries.__next__()
                 qs = qs.filter(Q(views__gte=x[1]) & Q(views__lt=z[1]))
-            if x[0].__contains__('category'):
-                qs = qs.filter(category__name__icontains=x[1])
-                
+            if x[0] == 'category' and x[1] != "Choose...":
+                qs = qs.filter(categories__name=x[1])  
         except StopIteration:
             break
     
-    return render(request, "form.html", {"qs": qs, c: categories})
+    return render(request, "form.html", {"qs": qs, "categories": categories, "checker": request.GET.get("reviewed")})
